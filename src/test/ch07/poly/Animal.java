82ad9b610f8bd3b3ed95638234d664005180695a
klina8575 <klina8575@gmail.com>
2022-11-18 18:40:11 +0900

    up

---
+++ src/test/ch07/poly/Animal.java
@@ -0,0 +1,18 @@
+package test.ch07.poly;
+
+public abstract class Animal {
+	//일반 메소드
+	public void breathe() {
+		System.out.println("숨을 쉽니다");
+	}
+	Animal() {
+		
+	}
+	
+	int a = 1;
+	
+	
+	//추상 메소드 선언
+	//★추상 메소드는 자식클래스에서 반드시 오버라이딩이 되어야 한다.
+	public abstract void sound();
+}
