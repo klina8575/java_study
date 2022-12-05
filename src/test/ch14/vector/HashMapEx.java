2800d5889e59ee604e7e553064adfce359c6e95c
klina8575 <klina8575@gmail.com>
2022-12-05 18:35:23 +0900

    up

--- src/test/ch14/vector/HashMapEx.java
+++ src/test/ch14/vector/HashMapEx.java
@@ -1,7 +1,10 @@
 package test.ch14.vector;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class HashMapEx {
 
@@ -14,6 +17,7 @@ public class HashMapEx {
 		map.put("김명현", 90);
 		map.put("양준수", 80);
 		map.put("노은비", 95);
+		map.put("노은비", 90); //키는 하나여야 한다.
 		
 		System.out.println("총 Entry 수: " + map.size());
 		
@@ -21,6 +25,32 @@ public class HashMapEx {
 		String key = "김명현";	
 		int value = map.get(key);
 		System.out.println("점수: " + value);
+		System.out.println();
+		
+		//반복해서 키와 값을 얻기 첫번째 방법
+		Set<String> keySet = map.keySet();
+		Iterator<String> keyIterator = keySet.iterator(); //반복된 키를 얻는다. 
+		
+		while (keyIterator.hasNext()) { //다음 키가 있는지 찾는다.
+			String k = keyIterator.next();
+			Integer v = map.get(k);
+			System.out.println(k + ":" + v); 
+		}
+		
+		System.out.println();
+		
+		//반복해서 키와 값을 얻기 두번째 방법
+		Set<Entry<String, Integer>> entrySet = map.entrySet();
+		Iterator<Entry<String, Integer>> entryIterator = entrySet.iterator(); //반복된 키를 얻는다.
+		
+		while (entryIterator.hasNext()) { //다음 키가 있는지 찾는다.
+			Entry<String, Integer> entry = entryIterator.next();
+			String k = entry.getKey(); //키를 가져옴
+			Integer v = entry.getValue(); //값을 가져옴
+			System.out.println(k + ":" + v); 
+		}
+		
+		
 		
 	}
 
